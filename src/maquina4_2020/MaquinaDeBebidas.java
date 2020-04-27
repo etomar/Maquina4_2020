e915c2c9a3566cc7d42349ddb98e95ad24f704e3
Marta Pretel <mpretel3@iesfuentesanluis.org>
2020-04-27 20:09:14 +0200

    Segunda implementacion maquina de bebidas

--- src/maquina4_2020/MaquinaDeBebidas.java
+++ src/maquina4_2020/MaquinaDeBebidas.java
@@ -2,36 +2,99 @@
 package maquina4_2020;
 class MaquinaDeBebidas
 {
-	//private ContadorDeMonedas contador;
-	//private DispensadorDeBotes cola,limon, naranja, tonica, agua;
-	private float precio; 
-	/* Inicializa la maquina y todos los elementos asociados */
-	/* Entradas: float m, cantidad inicial de monedas para el
-	cambio */
-	/* int b, cantidad inicial de botes en los dispensadores */
-	/* float pvp, precio de las bebidas */
-	/* Salidas: Ninguna */
-	public void iniciarMaquinaDeBebidas (float m, int b, float pvp) {};
-	/* Responder a una acción del usuario. Discrimina el tipo de
-	accion */
-	/* y utiliza las operaciones privadas */
-	/* Entradas: char o, la orden del usuario */
-	/* Salidas: Ninguna */
-	public void darOrden (char o) {};
-	/* Visualiza la informacion de interes sobre la maquina */
-	/* Botes en cada dispensador, precio de las bebidas, saldo
-	del cliente */
-	/* Entradas: Ninguna */
-	/* Salidas: Ninguna */
-	public void visualizarMaquina () {};
-	/* Realiza las acciones relacionadas con la introducción de
-	monedas */
-	/* Entradas: char o, la orden del usuario */
-	/* Salidas: Ninguna */
-	private void ordenMonedas (char o) {};
-	/* Realiza las acciones relacionadas con la selección de una
-	bebida */
-	/* Entradas: char o, la orden del usuario */
-	/* Salidas: Ninguna */
-	private void ordenSeleccion (char o) {};
+    private DispensadorDeBotes cola,limon, naranja, tonica, agua;
+    private float precio;
+
+    public void iniciarMaquinaDeBebidas (float m, int b, float pvp){
+    cola = new DispensadorDeBotes(b);
+    limon = new DispensadorDeBotes(b);
+    naranja = new DispensadorDeBotes(b);
+    tonica = new DispensadorDeBotes(b);
+    agua = new DispensadorDeBotes(b);
+    precio = pvp;   
+    }
+    public void darOrden (char o){        
+        if(Character.isDigit(o)){
+            ordenSeleccion(o);
+        }else{
+            ordenMonedas(o);
+        }
+    }
+
+    public void visualizarMaquina (){
+        System.out.println(
+                    "Cola: " + cola.botesDisponibles() + 
+                    " Naranja: " + naranja.botesDisponibles() +
+                    " Limon: " + limon.botesDisponibles() +
+                    " Tonica: " + tonica.botesDisponibles() +
+                    " Agua: " + agua.botesDisponibles() + 
+                    " Precio: " +precio);        
+    } 
+
+    private void ordenMonedas (char o){
+        
+        
+        if(o == 'A'){contador.insertarMoneda(0.05);}
+        else if(o == 'B'){contador.insertarMoneda(0.10);}
+        else if(o == 'C'){contador.insertarMoneda(0.20);}
+        else if(o == 'D'){contador.insertarMoneda(0.50);}
+        else if(o == 'E'){contador.insertarMoneda(1);}
+        else if(o == 'R'){contador.insertarMoneda(-contador.saldo());}
+        
+       
+        
+    }
+    private void ordenSeleccion (char o){
+        if(o == 1){
+            if(cola.pulsarBoton()){
+                    if(precio<=contador.Saldo()){
+                        cola.iniciarDispensadorDeBotes(cola.botesDisponibles ()-1);
+                    }else{
+                        System.out.println("No hay saldo");
+                    }
+                }else{
+                    System.out.println("No hay bebidas.");
+                }}
+        else if(o == 2){
+            if(limon.pulsarBoton()){
+                    if(precio<=contador.Saldo()){
+                        limon.iniciarDispensadorDeBotes(limon.botesDisponibles ()-1);
+                    }else{
+                        System.out.println("No hay saldo");
+                    }
+                }else{
+                    System.out.println("No hay bebidas.");
+                }}
+        else if(o == 3){ 
+            if(naranja.pulsarBoton()){
+                    if(precio<=contador.Saldo()){
+                        naranja.iniciarDispensadorDeBotes(naranja.botesDisponibles ()-1);
+                    }else{
+                        System.out.println("No hay saldo");
+                    }
+                }else{
+                    System.out.println("No hay bebidas.");
+                }}
+        else if(o == 4){
+            if(tonica.pulsarBoton()){
+                    if(precio<=contador.Saldo()){
+                        tonica.iniciarDispensadorDeBotes(tonica.botesDisponibles ()-1);
+                    }else{
+                        System.out.println("No hay saldo");
+                    }
+                }else{
+                    System.out.println("No hay bebidas.");
+                }}
+        else if(o == 5){
+            if(agua.pulsarBoton()){
+                    if(precio<=contador.Saldo()){
+                        agua.iniciarDispensadorDeBotes(agua.botesDisponibles ()-1);
+                    }else{
+                        System.out.println("No hay saldo");
+                    }
+                }else{
+                    System.out.println("No hay bebidas.");
+                }}
+        
+    }
 }
